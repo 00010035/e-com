058712a84cfc238db5b6fee4381c3a4dc5e52902
00010035 <72904930+00010035@users.noreply.github.com>
2023-01-21 16:36:58 +0500

    Setup React

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import header from "./components/header";
+import footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <header />
+      <main>
+        <h1>Welcome to FlatUz</h1>
+      </main>
+      <footer />
+    </>
+  );
+}
+
+export default App;
