26d037e7936035ea96650f9b4a1a690dd9f70c29
00010035 <72904930+00010035@users.noreply.github.com>
2023-03-16 00:22:43 +0500

    init: backend part

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,27 +1,39 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import SearchAppBar from "./searchbtn.js";
 // import "../variables.scss";
-import Container from "react-bootstrap/Container";
 
 const header = () => {
   return (
     <header>
       <Navbar bg="light" variant="light" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img style={{ width: "13rem" }} src="/logo/logo_4.png "></img>
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img style={{ width: "13rem" }} src="/logo/logo_4.png "></img>
+            </Navbar.Brand>
+          </LinkContainer>
           <Nav className="ml-auto">
-            <Nav.Link href="/rent">
-              {" "}
-              <i className="fa-solid fa-house"></i> Rent
+            <Nav.Link>
+              <SearchAppBar />
             </Nav.Link>
-            <Nav.Link href="/student">For Student</Nav.Link>
-            <Nav.Link href="/login">
-              {" "}
-              <i className="fas fa-user"></i> Sign In
-            </Nav.Link>
+            <LinkContainer to="/rent">
+              <Nav.Link>
+                {" "}
+                <i className="fa-solid fa-house"></i> Rent
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/student">
+              <Nav.Link>For Student</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                {" "}
+                <i className="fas fa-user"></i> Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Container>
       </Navbar>
